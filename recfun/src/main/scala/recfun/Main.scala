37e51f5c1c70c24e77a2ba786344d10afdbed8a2
Minjae Gwon <beta.underalpha@gmail.com>
2023-09-07 18:24:09 +0900

    recfun: Implement `balance`

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -24,7 +26,28 @@ object Main {
 
   /** Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def nextCount(char: Char, count: Int): Int = char match {
+      case '(' => count + 1
+      case ')' => count - 1
+      case _   => count
+    }
+
+    @tailrec
+    def isBalance(chars: List[Char], count: Int, flag: Boolean): Boolean =
+      (chars, count) match {
+        case (Nil, _)                                        => flag
+        case (head :: _, count) if head == ')' && count <= 0 => false
+        case (head :: tail, count) =>
+          isBalance(
+            tail,
+            nextCount(head, count),
+            nextCount(head, count) == 0
+          )
+      }
+
+    isBalance(chars, 0, flag = false)
+  }
 
   /** Exercise 3
     */
