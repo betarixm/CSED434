7403d3473af0d71cfd8d414d06ffeb1330a3b51f
Minjae Gwon <beta.underalpha@gmail.com>
2023-09-17 21:24:07 +0900

    patmat: Implement `createCodeTree`

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -141,7 +141,10 @@ object Huffman {
     * The parameter `chars` is an arbitrary text. This function extracts the character
     * frequencies from that text and creates a code tree based on them.
     */
-  def createCodeTree(chars: List[Char]): CodeTree = ???
+  def createCodeTree(chars: List[Char]): CodeTree =
+    until(singleton, combine)(
+      (times _ andThen makeOrderedLeafList)(chars)
+    ).head
 
   // Part 3: Decoding
 
