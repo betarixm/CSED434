f8bcb4c95c1b823d58d65276ce6f12d9daa2619b
Minjae Gwon <beta.underalpha@gmail.com>
2023-09-17 21:20:35 +0900

    patmat: Implement `times`

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -78,7 +78,11 @@ object Huffman {
     *       println("integer is  : "+ theInt)
     *   }
     */
-  def times(chars: List[Char]): List[(Char, Int)] = ???
+  def times(chars: List[Char]): List[(Char, Int)] =
+    chars
+      .groupBy(x => x)
+      .map({ case (c, chars) => (c, chars.size) })
+      .toList
 
   /** Returns a list of `Leaf` nodes for a given frequency table `freqs`.
     *
