c71e839cb2fd543fb39c8c08a4ded112e18852a0
Minjae Gwon <beta.underalpha@gmail.com>
2023-09-20 20:44:44 +0900

    streams: Implement `pathsFromStart`

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -86,7 +86,8 @@ trait Solver extends GameDef {
 
   /** The stream of all paths that begin at the starting block.
     */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] =
+    from(Stream(startBlock -> List()), Set())
 
   /** Returns a stream of all possible pairs of the goal block along
     * with the history how it was reached.
